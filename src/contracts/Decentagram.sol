903fda88ef665829a0156f42fc07b1d2d3d9f138
dienoov <alvinodien@yahoo.co.id>
2022-03-03 10:55:21 +0700

    upload image contract

--- src/contracts/Decentagram.sol
+++ src/contracts/Decentagram.sol
@@ -2,4 +2,35 @@ pragma solidity ^0.5.4;
 
 contract Decentagram {
     string public name = 'Decentagram';
+
+    uint public imageCount = 0;
+    mapping(uint => Image) public images;
+
+    struct Image {
+        uint id;
+        string hash;
+        string description;
+        uint tipAmount;
+        address payable author;
+    }
+
+    event ImageCreated(
+        uint id,
+        string hash,
+        string description,
+        uint tipAmount,
+        address payable author
+    );
+
+    function uploadImage(string memory _imgHash, string memory _description) public {
+        require(bytes(_imgHash).length > 0);
+        require(bytes(_description).length > 0);
+
+        require(msg.sender != address(0x0));
+
+        imageCount += 1;
+        images[imageCount] = Image(imageCount, _imgHash, _description, 0, msg.sender);
+
+        emit ImageCreated(imageCount, _imgHash, _description, 0, msg.sender);
+    }
 }
--- test/test.js
+++ test/test.js
@@ -25,4 +25,37 @@ contract('Decentagram', ([deployer, author, tipper]) => {
             assert.equal(name, 'Decentagram');
         });
     });
+
+    describe('images', async () => {
+        let result;
+        let imageCount;
+        const hash = 'abc123';
+
+        before(async () => {
+            result = await decentagram.uploadImage(hash, 'Image Description', {from: author});
+            imageCount = await decentagram.imageCount();
+        });
+
+        it('should create images', async () => {
+            assert.equal(imageCount, 1);
+            const event = result.logs[0].args;
+            assert.equal(event.id.toNumber(), imageCount.toNumber(), 'id is correct');
+            assert.equal(event.hash, hash, 'hash is correct');
+            assert.equal(event.description, 'Image Description', 'description is correct');
+            assert.equal(event.tipAmount, '0', 'tip amount is correct');
+            assert.equal(event.author, author, 'author is correct');
+
+            await decentagram.uploadImage('', 'Image Description', {from: author}).should.be.rejected;
+            await decentagram.uploadImage('Image hash', '', {from: author}).should.be.rejected;
+        });
+
+        it('should list images', async () => {
+            const image = await decentagram.images(imageCount);
+            assert.equal(image.id.toNumber(), imageCount.toNumber(), 'id is correct');
+            assert.equal(image.hash, hash, 'hash is correct');
+            assert.equal(image.description, 'Image Description', 'description is correct');
+            assert.equal(image.tipAmount, '0', 'tip amount is correct');
+            assert.equal(image.author, author, 'author is correct');
+        });
+    });
 });
